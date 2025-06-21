d94a25b0cceb0f351acfbbe22cf7c0441676d100
Willy Hu <willyhu.tw@gmail.com>
2025-06-21 22:30:21 +0800

    refactor(firewall): import getFilterLogs from another module.

--- src/tools/firewall.ts
+++ src/tools/firewall.ts
@@ -1,45 +1,11 @@
 import { tool } from "@langchain/core/tools";
 
-import * as T from "./types.js";
+import { getFilterLogs } from "../funcs/firewall.js";
+import * as T from "../types.js";
 import * as M from "./mocks.js";
 
 import dotenv from "dotenv";
 dotenv.config();
-
-const lokiApiUrl =
-  process.env.LOKI_API_URL || "http://localhost:3100/loki/api/v1";
-
-const getFilterLogs = async (
-  input: T.getFilterLogsIn,
-): Promise<T.getFilterLogsOut> => {
-  const start = new Date(input.start).getTime() / 1000;
-  const end = new Date(input.end).getTime() / 1000;
-
-  const resp = await fetch(
-    `${lokiApiUrl}/query_range?query=${encodeURIComponent('{log_type="filterlog", real_interface="pppoe0", action="block"}')}&start=${encodeURIComponent(start)}&end=${encodeURIComponent(end)}&limit=${encodeURIComponent(input.limit || 5000)}`,
-  );
-
-  const rawData: T.lokiQueryOut = await resp.json();
-
-  const records: T.blockedRecord[] = rawData.data.result.map((r) => {
-    return {
-      srcAddress: r.stream.source_address,
-      srcCountry: r.stream.source_country,
-      dstPort: parseInt(r.stream.destination_port, 10),
-    };
-  });
-
-  const result: T.getFilterLogsOut = {
-    timeRange: {
-      start: input.start,
-      end: input.end,
-    },
-    records,
-    total: rawData.data.result.length,
-  };
-
-  return result;
-};
 
 export const FirewallTool = tool(
   async (args: T.getFilterLogsIn) => {
